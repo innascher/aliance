991ed270bbb142ad06d4e02f4b95fad8c1de4a96
innascher <innascherk@gmail.com>
2023-01-09 15:34:33 +0400

    adaptive for small screens

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const navbar = document.querySelector(".navbar");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
   } else {
-    navbar.classList.remove("navbar-light");}
-
+    navbar.classList.remove("navbar-light")};
 })
+
+mMenuToggle.addEventListener("click", (Event) => {
+  Event.preventDefault();
+  menu.classList.toggle ("is-open");
+});
