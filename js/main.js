ce24af765060ca1feeee1c4b283263b036217d9e
innascher <innascherk@gmail.com>
2023-01-09 20:27:06 +0400

    fixed mobile menu

--- js/main.js
+++ js/main.js
@@ -2,14 +2,33 @@ const navbar = document.querySelector(".navbar");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
+const lightModeOn = (Event) => {
+  navbar.classList.add("navbar-light");
+}
+
+const lightModeOff = (Event) => {
+  navbar.classList.remove("navbar-light");
+}
+
+const openMenu = (Event) => {
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden";
+  lightModeOn();
+};
+
+const closeMenu = (Event) => {
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = "";
+  lightModeOff();
+};
+
 window.addEventListener("scroll", () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-  } else {
-    navbar.classList.remove("navbar-light")};
-})
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  });
 
 mMenuToggle.addEventListener("click", (Event) => {
   Event.preventDefault();
-  menu.classList.toggle ("is-open");
-});
+  menu.classList.contains('is-open') ? closeMenu() : openMenu();
+  });
