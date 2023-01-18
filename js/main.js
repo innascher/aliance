b2124157a06625a58cfe35117d599ee1f7e2d1f2
innascher <innascherk@gmail.com>
2023-01-18 19:03:34 +0400

    added: marks-section

--- js/main.js
+++ js/main.js
@@ -33,9 +33,10 @@ mMenuToggle.addEventListener("click", (Event) => {
   menu.classList.contains('is-open') ? closeMenu() : openMenu();
   });
 
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.features-slider', {
+    speed: 400,
     direction: 'horizontal',
-    slidesPerView: 5,
+    slidesPerView: 1,
     navigation: {
       nextEl: '.slider-button-next',
       prevEl: '.slider-button-prev',
@@ -60,26 +61,24 @@ mMenuToggle.addEventListener("click", (Event) => {
     }
   });
 
-  const stepsswiper = new Swiper('.steps-swiper', {
-    direction: 'horizontal',
+  const swiperSteps = new Swiper('.steps-slider', {
     slidesPerView: 1,
     navigation: {
       nextEl: '.steps-next-btn',
       prevEl: '.steps-prev-btn',
-    },
+  },
 
-    breakpoints: {
-      200: {
-        slidesPerView: 1,
-      },
-      576: {
-        slidesPerView: 2,
-      },
-      1214: {
-        slidesPerView: 3,
-      },
-      1243: {
-        slidesPerView: 4,
-      },
-    }
-  });
+breakpoints: {
+  576: {
+    slidesPerView: 1,
+  },
+  768: {
+    slidesPerView: 2,
+  },
+  1243: {
+    slidesPerView: 3,
+  },
+}
+});
+
+  
